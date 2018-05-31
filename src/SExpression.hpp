df7c2a3596c09f434665887c56532e8bb6400a33
xumenger <xumenger@126.com>
2018-05-31 17:38:47 +0800

    build asbtract syntax tree comments

--- src/SExpression.hpp
+++ src/SExpression.hpp
@@ -51,14 +51,17 @@ SExpression *ParseAsIScheme(String code){
     StrVector v = Tokenize(code);
     int i = 0;
     for(i = 0; i < v.size(); i++){
+        // 碰到左括号，创建一个新的节点为当前节点，然后重设当前节点
         if(v[i] == "("){
             SExpression *newNode = new SExpression("(", current);
             current->Children.push_back(newNode);
             current = newNode;
         }
+        // 碰到右括号，回退到当前节点的父节点
         else if(v[i] == ")"){
             current = current->Parent;
         }
+        // 否则为当前词素创建节点，并添加到当前节点的Children中
         else{
             SExpression *newNode = new SExpression(v[i], current);
             current->Children.push_back(newNode);
